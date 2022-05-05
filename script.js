95527ba10fa8df260b130dacbd5a9d502c3b3b2c
AinanCawley <ainan.c.cawley@gmail.com>
2022-05-05 18:07:56 +0800

    Add grid creation for buttons in script.js. Add basic layout in index.html. Remove margin and padding globally in style.css

--- script.js
+++ script.js
@@ -22,3 +22,26 @@ const operate = function( operation, a, b )
 {
     return operation(a,b);
 };
+
+//DOM STUFF
+
+const calculatorContainer = document.getElementById("calculatorContainer");
+const calculatorDisplay = document.getElementById("calculatorDisplay");
+const calculatorButtons = document.getElementById("calculatorButtons");
+
+let buttonFragment = new DocumentFragment();
+
+for( let i = 0; i < 5; i++ )
+{
+    let buttonColumn = document.createElement("div");
+
+    for( let j = 0; j < 4; j++ )
+    {
+        let button = document.createElement("div");
+        buttonColumn.appendChild(button);
+    };
+
+    buttonFragment.appendChild(buttonColumn);
+};
+
+calculatorButtons.appendChild(buttonFragment);
