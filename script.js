2ea73fdcafe0e11e012281aa9bbcc92dcddef2c2
AinanCawley <ainan.c.cawley@gmail.com>
2022-05-05 19:13:56 +0800

    Add button text content

--- script.js
+++ script.js
@@ -41,10 +41,114 @@ for( let i = 0; i < 5; i++ )
         let button = document.createElement("div");
         button.setAttribute("class", "calculatorButton");
         button.id = i + "x" + j;
+
+        if( i==0 )
+        {
+            if( j==0 )
+            {
+                button.id = "7button";
+                button.innerText = "7";
+            }
+            if( j==1 )
+            {
+                button.id = "4button";
+                button.innerText = "4";
+            }
+            if( j==2 )
+            {
+                button.id = "1button";
+                button.innerText = "1";
+            }
+        }
+        if( i==1 )
+        {
+            if( j==0 )
+            {
+                button.id = "8button";
+                button.innerText = "8";
+            }
+            if( j==1 )
+            {
+                button.id = "5button";
+                button.innerText = "5";
+            }
+            if( j==2 )
+            {
+                button.id = "2button";
+                button.innerText = "2";
+            }
+            if( j==3 )
+            {
+                button.id = "0button";
+                button.innerText = "0";
+            }
+        }
+        if( i==2 )
+        {
+            if( j==0 )
+            {
+                button.id = "9button";
+                button.innerText = "9";
+            }
+            if( j==1 )
+            {
+                button.id = "6button";
+                button.innerText = "6";
+            }
+            if( j==2 )
+            {
+                button.id = "3button";
+                button.innerText = "3";
+            }
+            if( j==3 )
+            {
+                button.id = "decimalButton";
+                button.innerText = ".";
+            }
+        }
+        if( i==3 )
+        {
+            if( j==0 )
+            {
+                button.id = "equalsButton";
+                button.innerText = "=";
+            }
+            if( j==1 )
+            {
+                button.id = "plusButton";
+                button.innerText = "+";
+            }
+            if( j==2 )
+            {
+                button.id = "timesButton";
+                button.innerText = "x";
+            }
+        }
+        if( i==4 )
+        {
+            if( j==0 )
+            {
+                button.id = "backspaceButton";
+                button.innerText = "backspace";
+            }
+            if( j==1 )
+            {
+                button.id = "minusButton";
+                button.innerText = "-";
+            }
+            if( j==2 )
+            {
+                button.id = "divideButton";
+                button.innerText = "/";
+            }
+        }
+
         buttonColumn.appendChild(button);
     };
 
     buttonFragment.appendChild(buttonColumn);
 };
+
+
 
 calculatorButtons.appendChild(buttonFragment);
